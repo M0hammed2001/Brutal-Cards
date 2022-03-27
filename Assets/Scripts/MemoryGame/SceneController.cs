ab2c755134a918762f6ff097333e8e5af2f4918e
HighestAuto <Jonathan.at.spence@hotmail.com>
2022-03-27 16:27:20 +0100

    .

--- Assets/Scripts/MemoryGame/SceneController.cs
+++ Assets/Scripts/MemoryGame/SceneController.cs
@@ -8,6 +8,7 @@ namespace BrutalCards{
 
     public class SceneController : MonoBehaviour {
 
+<<<<<<< HEAD
         public const int gridRows = 3;
         public const int gridCols = 8;
         public const float offsetX = 2.3f;
@@ -24,15 +25,23 @@ namespace BrutalCards{
 >>>>>>> parent of aa6d9b0 (Updated sound)
 =======
 >>>>>>> parent of aa6d9b0 (Updated sound)
+=======
+
+>>>>>>> parent of ce74dd5 (Revert "Merge branch 'level-2-backup' into UI-fixes")
 
         [SerializeField] private MemoryCard originalCard;
         [SerializeField] private Sprite[] images;
+        [SerializeField]List<MemoryCard> aiCardsToPick = new List <MemoryCard>();
+        MemoryGame memoryGame;
         
-        Player localPlayer;
-        Player remotePlayer;
+        int randomNumber;
+
+        public Player localPlayer;
+        public Player remotePlayer;
+
 
         [SerializeField]
-        protected Player currentTurnPlayer;
+        public Player currentTurnPlayer;
 
         [SerializeField]
         ProtectedData protectedData;
@@ -44,20 +53,7 @@ namespace BrutalCards{
         }
         
 
-        public enum GameState
-        {
-            Idle,
-            GameStarted,
-            TurnStarted,
-            TurnSelectingCards,
-            CheckingPairs,
-            GameFinished
-        };
 
-        [SerializeField]
-        protected GameState gameState = GameState.Idle;
-
-        
         protected void Awake()
         {
             Debug.Log("base awake");
@@ -71,48 +67,23 @@ namespace BrutalCards{
             remotePlayer.IsAI = true;
 
         }
-         public void ShowOptionsPopover()
+
+
+        public void Start()
         {
-            PopoverBackground.SetActive(true);
-            OptionsPopover.SetActive(true);
-        }
-        void OnGUI(){
-            if (Input.GetKeyDown("escape")){
-                Debug.Log("KeyCode down: escape");
-                OnOptionsClicked();
-            }   
-        }
+            Vector3 startPos = originalCard.transform.position; //position set for the first card. the others have been ofset from this position
 
-        public void OnOptionsClicked()
-        {
-            Debug.Log("OnOptionsClicked");
-            ShowOptionsPopover();
-        }
-         public void OnLobbyButtonClicked()
-        {
-            // FindObjectOfType<AudioManager>().Play("Creeky Door");
-            Debug.Log("OnLobbyButtonClicked");
-            SceneManager.LoadScene("LobbyScene");
-        }
-        public void OnCancelClicked(){
-            PopoverBackground.SetActive(false);
-            OptionsPopover.SetActive(false);
+            SwitchTurn();
+            int[] numbers =  { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11};
+            protectedData.memoryGameArray = numbers;
+            ShuffleArray(); 
 
-        }
-
-        
-        private void Start()
-        {
-            Vector3 startPos = originalCard.transform.position; //The position of the first card. All other cards are offset from here.
-
-            int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11};
-            numbers = ShuffleArray(numbers); //This is a function we will create in a minute!
-
-            for(int i = 0; i < gridCols; i++)
+            for(int i = 0; i < Constants.gridCols; i++)
             {
-                for(int j = 0; j < gridRows; j++)
+                for(int j = 0; j < Constants.gridRows; j++)
                 {
                     MemoryCard card;
+
                     if(i == 0 && j == 0)
                     {
                         card = originalCard;
@@ -121,108 +92,23 @@ namespace BrutalCards{
                     {
                         card = Instantiate(originalCard) as MemoryCard;
                     }
-
-                    int index = j * gridCols + i;
-                    int id = numbers[index];
+                    aiCardsToPick.Add(card);
+                    int index = j * Constants.gridCols + i;
+                    int id = protectedData.memoryGameArray[index];
                     card.ChangeSprite(id, images[id]);
 
-                    float posX = (offsetX * i) + startPos.x;
-                    float posY = (offsetY * j) + startPos.y;
+                    float posX = (Constants.offsetX * i) + startPos.x;
+                    float posY = (Constants.offsetY * j) + startPos.y;
                     card.transform.position = new Vector3(posX, posY, startPos.z);
                 }
             }
-            gameState = GameState.GameStarted;
-
         }
 
-        public virtual void GameFlow(){
-            if (gameState > GameState.GameStarted)
-            {
-                x = 1;
-                if (x != 1)
-                {
-                    gameState = GameState.GameFinished;
-                }
-            }
-
-            switch (gameState)
-            {
-                case GameState.Idle:
-                    {
-                        Debug.Log("IDLE");
-                        OnGameStarted();
-                        break;
-                    }
-                case GameState.GameStarted:
-                    {
-                        Debug.Log("GameStarted");
-                        OnturnStarted();
-                        break;
-                    }
-                case GameState.TurnStarted:
-                    {
-                        Debug.Log("TurnStarted");
-                        OnturnStarted();
-                        break;
-                    }
-                case GameState.TurnSelectingCards:
-                    {
-                        Debug.Log("TurnSelectingCards");
-                        OnTurnSelectingCards();
-                        break;
-                    }
-                case GameState.CheckingPairs:
-                    {
-                        Debug.Log("CheckingPairs");
-                        OnCheckingPairs();
-                        break;
-                    }
-                case GameState.GameFinished:
-                    {
-                        Debug.Log("GameFinished");
-                        OnGameFinished();
-                        break;
-                    }
-            }
-        }
-
-        protected void OnGameStarted()
-        {
-
-            gameState = GameState.TurnStarted;
-        }
-
-        protected void OnturnStarted()
-        {
-            SwitchTurn();
-            gameState = GameState.TurnSelectingCards;
-        }
-
-        protected void OnTurnSelectingCards()
-        {
-            if (currentTurnPlayer == localPlayer)
-            {
-
-            }
-            if (currentTurnPlayer.IsAI)
-            {
-
-            }
-        }
-
-        protected void OnCheckingPairs()
-        {
-            //CheckMatch()
-        }
-
-        protected void OnGameFinished()
-        {
-
-        }
         //-------------------------------------------------------------------------------------------------------------------------------------------
 
-        private int[] ShuffleArray(int[] numbers)
+        private int[] ShuffleArray()
         {
+            int[] numbers = protectedData.memoryGameArray;
             int[] newArray = numbers.Clone() as int[];
             for(int i = 0; i < newArray.Length; i++)
             {
@@ -231,35 +117,42 @@ namespace BrutalCards{
                 newArray[i] = newArray[r];
                 newArray[r] = tmp;
             }
+            protectedData.memoryGameArray = newArray;
             return newArray;
         }
 
         //-------------------------------------------------------------------------------------------------------------------------------------------
 
+        private MemoryCard _firstRevealed;
+        private MemoryCard _secondRevealed;
+
+        public int bot_score = 0;
+        public int player_score = 0;
+        [SerializeField] public TextMesh botScore;
+        [SerializeField] public TextMesh playerScore;
+        [SerializeField] public TextMesh playersTurn;
+
+
         public void SwitchTurn(){
             if (currentTurnPlayer == null)
             {
                 currentTurnPlayer = localPlayer;
+                playersTurn.text = localPlayer + "'s Turn" ;
                 return;
+
             }
 
             if (currentTurnPlayer == localPlayer)
             {
                 currentTurnPlayer = remotePlayer;
+                playersTurn.text = remotePlayer.PlayerName + "'s Turn" ;
             }
             else
             {
                 currentTurnPlayer = localPlayer;
+                playersTurn.text = localPlayer.PlayerName + "'s Turn" ;
             }
         }
-
-        private MemoryCard _firstRevealed;
-        private MemoryCard _secondRevealed;
-
-        private int bot_score = 0;
-        private int player_score = 0;
-        [SerializeField] private TextMesh botScore;
-        [SerializeField] private TextMesh playerScore;
 
         public bool canReveal
         {
@@ -270,19 +163,41 @@ namespace BrutalCards{
         {
             if(_firstRevealed == null)
             {
-                
                 _firstRevealed = card;
-                audioSource.PlayOneShot(pick, 1f);
             }
             else
             {
                 _secondRevealed = card;
                 StartCoroutine(CheckMatch());
-                audioSource.PlayOneShot(pick, 1f);
             }
         }
 
-        private IEnumerator CheckMatch()
+        public void AiCardpick()
+        {
+            int r = Random.Range(0, aiCardsToPick.Count);
+            int t = Random.Range(0, aiCardsToPick.Count);
+            while (r == t)
+            {
+                t = Random.Range(0, aiCardsToPick.Count);
+            }
+            CardRevealed(aiCardsToPick[r]);
+
+            
+        }
+
+        public bool CheckingMatch()
+        {
+            if(_firstRevealed.id == _secondRevealed.id) 
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public IEnumerator CheckMatch()
         {
             if(_firstRevealed.id == _secondRevealed.id)
             {
@@ -311,10 +226,6 @@ namespace BrutalCards{
 
         }
 
-        // public void Restart()
-        // {
-        //     SceneManager.LoadScene("Scene_001");
-        // }
-
+    
     }
 }
