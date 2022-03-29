81f3862b8c5f3b86d39ae3258da64f3a2f6201d1
Mohammed Sarhan <200150484@aston.ac.uk>
2022-03-29 16:34:09 +0100

    Revert "Revert "making secure data, broken currently""
    This reverts commit 0ca55b54287c1d9a4a00d3ab5c933d460ebb6f06.

--- Assets/Scripts/ProtectedData.cs
+++ Assets/Scripts/ProtectedData.cs
@@ -36,7 +36,7 @@ namespace BrutalCards
         int selectedRank;
         
         [SerializeField]
-        public int[] memoryGameArray;
+        List<byte> memoryGameArray = new List<byte>();
 
         byte[] encryptionKey;
         byte[] safeData;
@@ -58,6 +58,8 @@ namespace BrutalCards
             Encrypt();
         }
 
+        
+
         public List<byte> GetPoolOfCards()
         {
             List<byte> result;
@@ -67,6 +69,21 @@ namespace BrutalCards
             return result;
         }
 
+        public void SetMemoryCards(List<byte> cardValues)
+        {
+            Decrypt();
+            memoryGameArray = cardValues;
+            Encrypt();
+        }
+
+        public List<byte> GetMemoryCards()
+        {
+            List<byte> result;
+            Decrypt();
+            result = memoryGameArray;
+            Encrypt();
+            return result;
+        }
         public List<byte> PlayerCards(Player player)
         {
             List<byte> result;
@@ -267,6 +284,9 @@ namespace BrutalCards
             message.Push((Byte)poolOfCards.Count);
             message.PushByteArray(poolOfCards.ToArray());
 
+            message.Push((Byte)memoryGameArray.Count);
+            message.PushByteArray(memoryGameArray.ToArray());
+
             message.Push((Byte)player1Cards.Count);
             message.PushByteArray(player1Cards.ToArray());
 
@@ -290,6 +310,7 @@ namespace BrutalCards
             safeData = AES.EncryptAES128(message.ToArray(), encryptionKey);
 
             poolOfCards = new List<byte>();
+            memoryGameArray = new List<byte>();
             player1Cards = new List<byte>();
             player2Cards = new List<byte>();
             booksForPlayer1 = new List<byte>();
@@ -308,6 +329,9 @@ namespace BrutalCards
             SWNetworkMessage message = new SWNetworkMessage(byteArray);
             byte poolOfCardsCount = message.PopByte();
             poolOfCards = message.PopByteArray(poolOfCardsCount).ToList();
+
+            byte memoryGameArray = message.PopByte();
+            poolOfCards = message.PopByteArray(memoryGameArray).ToList();
 
             byte player1CardsCount = message.PopByte();
             player1Cards = message.PopByteArray(player1CardsCount).ToList();
