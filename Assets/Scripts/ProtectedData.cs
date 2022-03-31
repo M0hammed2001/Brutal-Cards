3d03dc0b41d059aaa64fb2c3a58adefcbf2cba9b
HighestAuto <Jonathan.at.spence@hotmail.com>
2022-03-31 15:44:24 +0100

    clearing up code

--- Assets/Scripts/ProtectedData.cs
+++ Assets/Scripts/ProtectedData.cs
@@ -48,29 +48,29 @@ namespace BrutalCards
             currentTurnPlayerId = "";
             selectedRank = (int)Ranks.NoRanks;
             CalculateKey(roomId);
-            Encrypt();
+            
         }
 
         public void SetPoolOfCards(List<byte> cardValues)
         {
-            Decrypt();
+            
             poolOfCards = cardValues;
-            Encrypt();
+             
         }
 
         public List<byte> GetPoolOfCards()
         {
             List<byte> result;
-            Decrypt();
+             
             result = poolOfCards;
-            Encrypt();
+             
             return result;
         }
 
         public List<byte> PlayerCards(Player player)
         {
             List<byte> result;
-            Decrypt();
+             
             if (player.PlayerId.Equals(player1Id))
             {
                 result = player1Cards;
@@ -79,14 +79,14 @@ namespace BrutalCards
             {
                 result = player2Cards;
             }
-            Encrypt();
+             
             return result;
         }
 
         public List<byte> PlayerBooks(Player player)
         {
             List<byte> result;
-            Decrypt();
+             
             if (player.PlayerId.Equals(player1Id))
             {
                 result = booksForPlayer1;
@@ -95,13 +95,13 @@ namespace BrutalCards
             {
                 result = booksForPlayer2;
             }
-            Encrypt();
+             
             return result;
         }
 
         public void AddCardValuesToPlayer(Player player, List<byte> cardValues)
         {
-            Decrypt();
+             
             if (player.PlayerId.Equals(player1Id))
             {
                 player1Cards.AddRange(cardValues);
@@ -112,12 +112,12 @@ namespace BrutalCards
                 player2Cards.AddRange(cardValues);
                 player2Cards.Sort();
             }
-            Encrypt();
+             
         }
 
         public void AddCardValueToPlayer(Player player, byte cardValue)
         {
-            Decrypt();
+             
             if (player.PlayerId.Equals(player1Id))
             {
                 player1Cards.Add(cardValue);
@@ -128,12 +128,12 @@ namespace BrutalCards
                 player2Cards.Add(cardValue);
                 player2Cards.Sort();
             }
-            Encrypt();
+             
         }
 
         public void RemoveCardValuesFromPlayer(Player player, List<byte> cardValuesToRemove)
         {
-            Decrypt();
+             
             if (player.PlayerId.Equals(player1Id))
             {
                 player1Cards.RemoveAll(cv => cardValuesToRemove.Contains(cv));
@@ -142,12 +142,12 @@ namespace BrutalCards
             {
                 player2Cards.RemoveAll(cv => cardValuesToRemove.Contains(cv));
             }
-            Encrypt();
+             
         }
 
         public void AddBooksForPlayer(Player player, Ranks ranks)
         {
-            Decrypt();
+             
             if (player.PlayerId.Equals(player1Id))
             {
                 booksForPlayer1.Add((byte)ranks);
@@ -156,13 +156,13 @@ namespace BrutalCards
             {
                 booksForPlayer2.Add((byte)ranks);
             }
-            Encrypt();
+             
         }
 
         public bool GameFinished()
         {
             bool result = false;
-            Decrypt();
+             
             if (poolOfCards.Count == 0)
             {
                 result = true;
@@ -177,7 +177,7 @@ namespace BrutalCards
             {
                 result = true;
             }
-            Encrypt();
+             
 
             return result;
         }
@@ -185,7 +185,7 @@ namespace BrutalCards
         public string WinnerPlayerId()
         {
             string result;
-            Decrypt();
+             
             if (booksForPlayer1.Count > booksForPlayer2.Count)
             {
                 result = player1Id;
@@ -194,54 +194,54 @@ namespace BrutalCards
             {
                 result = player2Id;
             }
-            Encrypt();
+             
             return result;
         }
 
         public void SetCurrentTurnPlayerId(string playerId)
         {
-            Decrypt();
+             
             currentTurnPlayerId = playerId;
-            Encrypt();
+             
         }
 
         public string GetCurrentTurnPlayerId()
         {
             string result;
-            Decrypt();
+             
             result = currentTurnPlayerId;
-            Encrypt();
+             
             return result;
         }
 
         public void SetGameState(int gameState)
         {
-            Decrypt();
+             
             currentGameState = gameState;
-            Encrypt();
+             
         }
         public int GetGameState()
         {
             int result;
-            Decrypt();
+             
             result = currentGameState;
-            Encrypt();
+             
             return result;
         }
 
         public void SetSelectedRank(int rank)
         {
-            Decrypt();
+             
             selectedRank = rank;
-            Encrypt();
+             
         }
 
         public int GetSelectedRank()
         {
             int result;
-            Decrypt();
+             
             result = selectedRank;
-            Encrypt();
+             
             return result;
         }
 
@@ -261,73 +261,5 @@ namespace BrutalCards
             encryptionKey = Encoding.UTF8.GetBytes(roomIdSubString);
         }
 
-        void Encrypt()
-        {
-            SWNetworkMessage message = new SWNetworkMessage();
-            message.Push((Byte)poolOfCards.Count);
-            message.PushByteArray(poolOfCards.ToArray());
-
-            message.Push((Byte)player1Cards.Count);
-            message.PushByteArray(player1Cards.ToArray());
-
-            message.Push((Byte)player2Cards.Count);
-            message.PushByteArray(player2Cards.ToArray());
-
-            message.Push((Byte)booksForPlayer1.Count);
-            message.PushByteArray(booksForPlayer1.ToArray());
-
-            message.Push((Byte)booksForPlayer2.Count);
-            message.PushByteArray(booksForPlayer2.ToArray());
-
-            message.PushUTF8ShortString(player1Id);
-            message.PushUTF8ShortString(player2Id);
-
-            message.PushUTF8ShortString(currentTurnPlayerId);
-            message.Push(currentGameState);
-
-            message.Push(selectedRank);
-
-            safeData = AES.EncryptAES128(message.ToArray(), encryptionKey);
-
-            poolOfCards = new List<byte>();
-            player1Cards = new List<byte>();
-            player2Cards = new List<byte>();
-            booksForPlayer1 = new List<byte>();
-            booksForPlayer2 = new List<byte>();
-            player1Id = null;
-            player2Id = null;
-            currentTurnPlayerId = null;
-            currentGameState = 0;
-            selectedRank = 0;
-        }
-
-        void Decrypt()
-        {
-            byte[] byteArray = AES.DecryptAES128(safeData, encryptionKey);
-
-            SWNetworkMessage message = new SWNetworkMessage(byteArray);
-            byte poolOfCardsCount = message.PopByte();
-            poolOfCards = message.PopByteArray(poolOfCardsCount).ToList();
-
-            byte player1CardsCount = message.PopByte();
-            player1Cards = message.PopByteArray(player1CardsCount).ToList();
-
-            byte player2CardsCount = message.PopByte();
-            player2Cards = message.PopByteArray(player2CardsCount).ToList();
-
-            byte booksForPlayer1Count = message.PopByte();
-            booksForPlayer1 = message.PopByteArray(booksForPlayer1Count).ToList();
-
-            byte booksForPlayer2Count = message.PopByte();
-            booksForPlayer2 = message.PopByteArray(booksForPlayer2Count).ToList();
-
-            player1Id = message.PopUTF8ShortString();
-            player2Id = message.PopUTF8ShortString();
-
-            currentTurnPlayerId = message.PopUTF8ShortString();
-            currentGameState = message.PopInt32();
-
-            selectedRank = message.PopInt32();
-        }
     }
 }
